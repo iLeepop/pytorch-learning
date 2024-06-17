6e9ae877d0d88adb43e8008b3727f1164db0a3ea
ileepop <zihao02325@gmail.com>
2024-06-17 15:30:09 +0800

    modify .gitignore

--- tests/totensor_test.py
+++ tests/totensor_test.py
@@ -1,9 +1,16 @@
 from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
 import cv2
 
 
 img_path = "../dataset/train/human_image/000000002007_jpg.rf.a19a160c6197a61e7e5ccdebc9767ae3.jpg"
 img = cv2.imread(img_path)
+
+writer = SummaryWriter("../logs")
+
 transform = transforms.ToTensor()
 img_tensor = transform(img)
-print(img_tensor)
+
+writer.add_image("Tensor", img_tensor)
+
+writer.close()
